a2c8e9a29cb5bd8d15827e8bb9e2d02810ef2851
Jesper Kaae <jk@webstarters.dk>
2019-01-08 09:09:29 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,10 @@
+<?php 
 add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields', 10, 1 );
 function custom_override_checkout_fields( $fields ) {
 
-    $fields['billing']['billing_city']['default'] = 'Beverly Hills';
+    $fields['billing']['billing_city']['default'] = 'Kolding';
     $fields['billing']['billing_postcode']['autocomplete'] = null; // Removing autocomplete
 
     return $fields;
 }
+?>
